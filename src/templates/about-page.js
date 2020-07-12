ec20344bec2adce710f2479176bd0fa2cbe768dc
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2020-07-12 17:53:53 +0100

    coming along!

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -24,12 +24,12 @@ const AboutPage = () => (
               <div className="col-lg-8 col-sm-12">
                 <h1 className=" h1 text-center">Our Vision</h1>
                 <p className="p">
-                  Kingdom Helps is a nondenominational initiative that renders
-                  help as God representatives on earth on behalf of God's
-                  Kingdom and our drive is to help till our last breath. Our
-                  passion and zeal is for the lost. Our strategy is to connect
-                  and reunite hearts back to God by teaching and impacting the
-                  sincere milk of God's word.
+                  We are daughters of Zion with the mandate of connecting and
+                  re-uniting hearts back to God. Saved to save others and the
+                  lost sheep; To save others from the shackles of darkness and
+                  also sent to the saved that are lost with the help of the Holy
+                  spirit working through the Charity, Self development, Health
+                  and Relatonship arm of Kingdom helps. Matthew 9:36
                 </p>
               </div>
             </div>
@@ -41,11 +41,12 @@ const AboutPage = () => (
               <div className="col-lg-8 col-sm-12">
                 <h1 className="h1 text-center">Our Mission</h1>
                 <p className="p">
-                  We are not denominational and never want to be. As we move
-                  faster to wiping away the cosmos, we intensify the need to
-                  break denominational barriers and bring as many million souls
-                  to the reality of heaven and hell thereby defeating the
-                  struggling lord of this world.
+                  Kingdom Helps is a nondenominational initiative that renders
+                  help as God representatives on earth on behalf of God's
+                  Kingdom and our drive is to help till our last breath. Our
+                  passion and zeal is for the lost. Our strategy is to connect
+                  and reunite hearts back to God by teaching and impacting the
+                  sincere milk of God's word.
                   <br />
                 </p>
               </div>
@@ -58,10 +59,10 @@ const AboutPage = () => (
               <div className="col-lg-8 col-sm-12">
                 <h1 className="h1 text-center">Our Values</h1>
                 <p className="p ">
-                  Help is our calling Holiness & Purity There is a message in
-                  every mess, we must find it and bless lives! Ready to help
-                  till our last breath Never giving up on a soul HolySpirit is
-                  our core value
+                  <p className="text-center">Help is our calling</p>
+                  Holiness & Purity! There is a message in every mess, we must
+                  find it and bless lives! Ready to help till our last breath,
+                  never giving up on a soul. Holy Spirit is our core value!
                 </p>
               </div>
             </div>
